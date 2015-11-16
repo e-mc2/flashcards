0f5fab17f010accc801e53d020048b065a0e8125
Dima Rogov <navydimka@gmail.com>
2015-11-16 21:38:10 +1300

    add new view index for cards controller, add resources routes for cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :cards, only: [:index, :create]
+
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
