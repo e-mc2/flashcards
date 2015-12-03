994cb0247a61437fe916969bb65871b0d93c151c
Dima Rogov <navydimka@gmail.com>
2015-12-03 13:38:33 +1300

    add new action and view, user can edit, update and delete cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,12 @@
 class CardsController < ApplicationController
+  before_action :find_card, only: [:show, :update, :destroy, :edit]
+
   def index
-    @cards = Card.all
+    @cards = Card.all.order("review_date ASC")
+  end
+
+  def show
+    
   end
 
   def new
@@ -11,16 +17,37 @@ class CardsController < ApplicationController
     @card = Card.new(cards_params)
 
     if @card.save
-      redirect_to cards_url, notice: "Successfully created new card"
+      redirect_to cards_path, notice: "Successfully created new card"
     else
       render 'new'
     end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @card.update(cards_params)
+      redirect_to cards_path
+    else
+      render 'edit'
+    end  
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to cards_url, notice: "Successfully delete card"
   end
 
   private
 
   def cards_params
     params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
   end
 
 end
